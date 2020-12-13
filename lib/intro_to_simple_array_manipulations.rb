cda9c9b61952b7cf089899aa527056a060e739fa
Jacob Hampton <jhampton792@gmail.com>
2020-12-13 20:09:21 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,33 @@
+def using_concat(arr_one, arr_two)
+  arr_one.concat(arr_two)
+end
+
+def using_insert(arr_one, element)
+  
+  arr_one.insert(4, element)
+
+end
+
+def using_uniq(arr)
+  
+  arr.uniq
+
+end
+
+def using_flatten (arr)
+  
+  arr.flatten
+  
+end
+
+def using_delete (arr, str)
+  
+  arr.delete(str)
+
+end
+
+def using_delete_at (arr, int)
+  
+  arr.delete_at(int)
+  
+end
